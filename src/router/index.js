79a390862e88d060037dd9d40a77c5d957f24331
Your Name Here <lawsonezekiel68@gmail.com>
2023-02-09 21:46:43 +0100

    change component name

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ShoppingCart from '../views/ShoppingCart.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'shoppingcart',
+      component: ShoppingCart
     },
    
   ]
